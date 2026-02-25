3c16d7700681d91797e3254effadc25f2525a2ce
desenvolvedor <desenvolvedor@986EAC49-5050-4>
2026-02-25 17:55:44 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto01/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                    }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int aa, an, idade;
+            aa = int.Parse(textBox3.Text);
+            an = int.Parse(textBox6.Text);
+            idade = aa - an;
+            textBox7.Text = idade.ToString() + " Anos.";
+            MessageBox.Show(textBox1.Text + " Sua idade é: " + idade.ToString() + " Anos",
+                "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
+
